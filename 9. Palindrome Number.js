a853186298c0fa803d8095bd187c3fb038ffcd58
V Manin <victormanin151@gmail.com>
2024-10-02 18:44:39 +0300

    problem number 9

---
+++ 9. Palindrome Number.js
@@ -0,0 +1,12 @@
+var isPalindrome = function(x) {
+    x = x.toString();
+    let reverse = x.split("").reverse().join("");
+    if(x === reverse){
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+
+console.log(isPalindrome(121));
