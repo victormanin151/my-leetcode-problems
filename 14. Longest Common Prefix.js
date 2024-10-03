631e5082b6a55e7e996c5d9fe59d1ec1bba6d631
V Manin <victormanin151@gmail.com>
2024-10-03 13:12:15 +0300

    problem N 14

---
+++ 14. Longest Common Prefix.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if(strs.length === 0){
+        return "";
+    }
+    let prefix = strs[0];;
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.slice(0, -1);
+            if (prefix === ""){
+                return "";
+            } 
+        }
+    }
+    return prefix;
+};
+
+
+let str = ["flower","flower","flower","flower"];
+console.log(longestCommonPrefix(str));
